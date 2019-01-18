be3767278045f81f9c941171dc4eda387418a997
JonathanPark <joohopark27@gmail.com>
2019-01-18 08:53:19 -0500

    Initial commit

---
+++ src/Drawable.java
@@ -0,0 +1,8 @@
+public interface Drawable {
+	
+	// Draws the object on DrawingBoard db
+	void drawOn(DrawingBoard db);
+	
+	// Returns true if the object falls entirely within DrawingBoard db
+	boolean within(DrawingBoard db);
+}
