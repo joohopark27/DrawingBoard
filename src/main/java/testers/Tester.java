c699d24e8a01b58e98cf694f6a57c7e51ed36216
JonathanPark <joohopark27@gmail.com>
2019-01-29 08:53:59 -0500

    masterpiece

--- src/main/java/testers/Tester.java
+++ src/main/java/testers/Tester.java
@@ -2,6 +2,9 @@ package main.java.testers;
 
 import main.java.Colors;
 import main.java.Drawing;
+import main.java.DrawingBoard;
+import main.java.Point;
+import main.java.drawable.shape.Polygon;
 
 import java.io.IOException;
 
@@ -13,6 +16,17 @@ public class Tester {
         Drawing drawing = new Drawing(filePath);
 
         //add tester code here
+        Polygon polygon = new Polygon();
+        polygon.fill(true);
+        polygon.changeColor(Colors.YELLOW);
+
+        for(int i = 0; i < 50; i++){
+
+            polygon.add(new Point(Math.random() * 800 + 10, Math.random() * 200 + 10));
+
+        }
+
+        drawing.add(polygon);
 
         drawing.show();
 
