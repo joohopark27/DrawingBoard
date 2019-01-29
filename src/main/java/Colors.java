043973b6dcd750b2bd53c89ab53e3034e6a423f1
JonathanPark <joohopark27@gmail.com>
2019-01-29 08:44:50 -0500

    added colors

--- src/main/java/Colors.java
+++ src/main/java/Colors.java
@@ -6,6 +6,10 @@ public enum Colors {
     BLUE (0x0000FF),
     GREEN (0x00FF00),
     BLACK (0x000000),
+    YELLOW (0xFFFF00),
+    AQUA (0x00FFFF),
+    NAVY (0x000080),
+    PURPLE (0x800080),
     WHITE (0xFFFFFF);
 
     private int val;
